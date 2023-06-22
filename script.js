090c461804148bc39b0597ef9a410a23cd0278d3
jow76 <joejardine76@gmail.com>
2023-06-22 14:24:58 +0100

    added form but doesnt work yet

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const hour = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
 const stores = ["Seattle", "Tokyo", "Dubai", "Paris", "Lima"]
 
+const storeForm = document.getElementById("addStoreForm");
+const allStores = [];
+
 const salesDiv = document.getElementById("container");
 let isTable=0;
 
@@ -14,57 +17,29 @@ function Store(storeName, minCustPerHour, maxCustPerHour, avgCookiesPerCust){
     this.customersEachHour = [];
     this.cookiesEachHour = [];
     this.totalDailyCookies = 0;
-    this.render()
+    this.pushStore = function(){
+        allStores.push(this)
+    }
+    this.pushStore;
 }
 
 Store.prototype.render = function(){
     this.calcCustomersEachHour();
     this.calcCookiesEachHour();
-    
-    const h3 = document.createElement("h3");
-    h3.textContent = this.storeName;
-    // container.appendChild(h3);
-    const ul = document.createElement("ul");
-    // container.appendChild(ul);
-    for(let i = 0; i < 14; i++){
-        const li = document.createElement("li");
-        li.textContent = `${hour[i]} :  ${this.cookiesEachHour[i]} cookies.`
-        ul.appendChild(li);
-    }
-    const li = document.createElement("li");
-    li.textContent = `Total : ${this.totalDailyCookies} cookies.`
-    ul.appendChild(li)
-    while(isTable==0){
-        const table = document.createElement("table")
-        const headerRow = document.createElement("tr")
-        const article = document.createElement("article");
-        container.appendChild(article);
-        table.appendChild(headerRow)
-        article.appendChild(table)
-        const storeHeaderCell = document.createElement("th")
-        storeHeaderCell.textContent="Store"
-        headerRow.appendChild(storeHeaderCell)
-        for(let i=0; i < hour.length; i++){
-            const hourHeaderCell = document.createElement("th")
-            hourHeaderCell.textContent = hour[i]
-            headerRow.appendChild(hourHeaderCell)
-        }
-        const hourHeaderCell = document.createElement("th")
-            hourHeaderCell.textContent = "Total"
-            headerRow.appendChild(hourHeaderCell)
-        isTable=1;
-    }
-    const storeNameData = document.createElement("tr")
-    storeNameData.textContent = this.storeName
-    table.appendChild(storeNameData)
-    for(let i=0; i < hour.length; i++){
-        const salesData = document.createElement("td")
-        salesData.textContent=this.cookiesEachHour[i]
-        storeNameData.appendChild(salesData)
+    const storeTable = document.getElementById("store-table")
+    const tr = document.createElement("tr")
+    const th = document.createElement("th")
+    th.textContent = this.storeName;
+    storeTable.appendChild(tr)
+    tr.appendChild(th);
+    for(let i =0; i < hour.length; i++){
+        const td = document.createElement("td")
+        td.textContent = this.cookiesEachHour[i]
+        tr.appendChild(td)
     }
     const salesData = document.createElement("td")
     salesData.textContent=this.totalDailyCookies
-    storeNameData.appendChild(salesData)
+    tr.appendChild(salesData)
 }
 
 Store.prototype.calcCustomersEachHour = function(){
@@ -89,3 +64,23 @@ const tokyo = new Store("Tokyo",3,24,1.2)
 const dubai = new Store("Dubai",11,38,3.7)
 const paris = new Store("Paris",20,38,2.3)
 const lima = new Store("Lima",2,16,4.6)
+
+storeForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    const storeName = event.target.name.value;
+    const minCustPerHour = event.target.minCust.value;
+    const maxCustPerHour = event.target.maxCust.value;
+    const avgCookiesPerCust = event.target.avgCookie.value;
+    const newStore = new Store(storeName, minCustPerHour, maxCustPerHour, avgCookiesPerCust)
+    console.log(allStores)
+    renderAllStores();
+    storeForm.reset();
+})
+
+function renderAllStores(){
+    for(let i = 0; i < allStores.length; i++){
+        allStores[i].render();
+    }
+}
+
+renderAllStores();
